28c92418ecc8d2625e50fcf63cea66f5ef8436d7
Rainer Kuemmerle <rainer.kuemmerle@gmail.com>
2025-02-16 11:38:14 +0100

    Implement debouncing of connection events

--- include/scronify/x11_event.h
+++ include/scronify/x11_event.h
@@ -1,11 +1,14 @@
 #pragma once
 
+#include <qmutex.h>
+#include <qobject.h>
 #include <qthread.h>
+#include <qtimer.h>
 
 #include <cstdint>
 #include <unordered_map>
 
-#include "scronify/output_connection.h"
+#include "scronify/event.h"
 
 namespace scronify {
 
@@ -21,11 +24,14 @@ class X11Event : public QThread {
   void ScreenRemoved();
 
  protected:
-  std::unordered_map<std::uint64_t, OutputConnection> cached_output_;
+  std::unordered_map<std::uint64_t, Event> cached_output_;
+  QMutex cached_output_mutex_;
 
   void run() override;
   void Connect(std::uint64_t output, const ScreenHandle& handle);
   void Removed(std::uint64_t output, const ScreenHandle& handle);
+  void SetupDebounce(std::uint64_t output, Event& event);
+  void Debounced(std::uint64_t output);
 };
 
 }  // namespace scronify
--- src/x11_event.cpp
+++ src/x11_event.cpp
@@ -1,18 +1,21 @@
 #include "scronify/x11_event.h"
 
+#include <qcoreapplication.h>
 #include <qdebug.h>
 #include <qlogging.h>
+#include <qobject.h>
+#include <qtimer.h>
 
 #include <memory>
 #include <string_view>
 
+#include "scronify/event.h"
 #include "scronify/moc_x11_event.cpp"  // NOLINT
 #include "scronify/output_connection.h"
 
 // must be last
 #include <X11/Xlib.h>
 #include <X11/extensions/Xrandr.h>
-#include <qobject.h>
 
 namespace {
 QString ConnectionToStr(int c) {
@@ -25,6 +28,10 @@ QString ConnectionToStr(int c) {
       return "unknown";
   }
 }
+
+constexpr int kSleepMs = 500;
+constexpr int kDebounceMs = 1000;
+
 }  // namespace
 
 namespace scronify {
@@ -47,85 +54,115 @@ void X11Event::run() {
   XRRSelectInput(display, DefaultRootWindow(display), RROutputChangeNotifyMask);
   XSync(display, False);
 
-  while (true) {
-    if (QThread::currentThread()->isInterruptionRequested()) {
-      return;
-    }
-
+  while (!QThread::currentThread()->isInterruptionRequested()) {
+    QCoreApplication::processEvents();
     const int num_events = XEventsQueued(display, QueuedAfterFlush);
     if (num_events <= 0) {
-      msleep(500);  // NOLINT
+      msleep(kSleepMs);
       continue;
     }
 
     for (int i = 0; i < num_events; ++i) {
       XEvent ev;
-      if (!XNextEvent(display, &ev)) {
-        auto* output_change_notify =
-            reinterpret_cast<XRROutputChangeNotifyEvent*>(&ev);  // NOLINT
+      if (XNextEvent(display, &ev)) {
+        qWarning("Error while fetching XNextEvent");
+        continue;
+      }
 
-        ScreenResourcePtr screen_resource(
-            XRRGetScreenResources(output_change_notify->display,
-                                  output_change_notify->window),
-            XRRFreeScreenResources);
-        if (screen_resource == nullptr) {
-          qWarning() << "Could not get screen resources";
-          continue;
-        }
+      auto* output_change_notify =
+          reinterpret_cast<XRROutputChangeNotifyEvent*>(&ev);  // NOLINT
 
-        OutputInfoPtr output_info(
-            XRRGetOutputInfo(output_change_notify->display,
-                             screen_resource.get(),
-                             output_change_notify->output),
-            XRRFreeOutputInfo);
-        if (output_info == nullptr) {
-          qWarning() << "Could not get output info";
-          continue;
-        }
+      ScreenResourcePtr screen_resource(
+          XRRGetScreenResources(output_change_notify->display,
+                                output_change_notify->window),
+          XRRFreeScreenResources);
+      if (screen_resource == nullptr) {
+        qWarning() << "Could not get screen resources";
+        continue;
+      }
 
-        QString output_name = QString(output_info->name).trimmed();
-        qDebug() << "Event for output " << output_change_notify->output << " "
-                 << output_name << " "
-                 << ConnectionToStr(output_info->connection);
+      OutputInfoPtr output_info(
+          XRRGetOutputInfo(output_change_notify->display, screen_resource.get(),
+                           output_change_notify->output),
+          XRRFreeOutputInfo);
+      if (output_info == nullptr) {
+        qWarning() << "Could not get output info";
+        continue;
+      }
 
-        const int connection = output_info->connection;
-        ScreenHandle screen_handle(std::move(screen_resource),
-                                   std::move(output_info));
-        switch (connection) {
-          case 0:  // connected
-            Connect(output_change_notify->output, screen_handle);
-            break;
-          case 1:  // disconnected
-            Removed(output_change_notify->output, screen_handle);
-            break;
-          default:
-            qWarning() << "Unknown connection type";
-        }
+      QString output_name = QString(output_info->name).trimmed();
+      qDebug() << "Event for output " << output_change_notify->output << " "
+               << output_name << " "
+               << ConnectionToStr(output_info->connection);
+
+      const int connection = output_info->connection;
+      ScreenHandle screen_handle(std::move(screen_resource),
+                                 std::move(output_info));
+      switch (connection) {
+        case 0:  // connected
+          Connect(output_change_notify->output, screen_handle);
+          break;
+        case 1:  // disconnected
+          Removed(output_change_notify->output, screen_handle);
+          break;
+        default:
+          qWarning() << "Unknown connection type";
       }
     }
   }
 }
 
 void X11Event::Connect(std::uint64_t output, const ScreenHandle& handle) {
+  QMutexLocker locker(&cached_output_mutex_);
   auto found_it = cached_output_.find(output);
 
   bool already_connected = found_it != cached_output_.end();
   const OutputConnection&
       connection =  // TODO(Rainer) generate correct connection
-      already_connected ? found_it->second : OutputConnection();
+      already_connected ? found_it->second.connection : OutputConnection();
 
-  cached_output_[output] = connection;
-
-  if (!already_connected) {
-    // TODO debounce
-    emit ScreenAdded();
-  }
+  cached_output_[output].connection = connection;
+  cached_output_[output].type = EventType::kConnected;
+  SetupDebounce(output, cached_output_[output]);
 }
 
 void X11Event::Removed(std::uint64_t output, const ScreenHandle& handle) {
-  // TODO remove
-  emit ScreenRemoved();
-  cached_output_.erase(output);
+  QMutexLocker locker(&cached_output_mutex_);
+  cached_output_[output].type = EventType::kRemoved;
+  SetupDebounce(output, cached_output_[output]);
+}
+
+void X11Event::SetupDebounce(std::uint64_t output, Event& event) {
+  if (!event.timer_connected) {
+    connect(event.timer, &QTimer::timeout, this,
+            [this, output] { this->Debounced(output); });
+    event.timer_connected = true;
+  }
+  event.timer->setSingleShot(true);
+  event.timer->setInterval(kDebounceMs);
+  event.timer->start();
+}
+
+void X11Event::Debounced(std::uint64_t output) {
+  QMutexLocker locker(&cached_output_mutex_);
+
+  auto found_it = cached_output_.find(output);
+  if (found_it == cached_output_.end()) {
+    qCritical() << "Logic error: Unable to find output's cache";
+    return;
+  }
+
+  switch (found_it->second.type) {
+    case EventType::kConnected:
+      emit ScreenAdded();
+      break;
+    case EventType::kRemoved:
+      emit ScreenRemoved();
+      break;
+    case EventType::kUnknown:
+      qCritical() << "Logic error: Unknown event type";
+      break;
+  }
 }
 
 }  // namespace scronify
